a1fda781e5e7e48861d711861e2ea64fa88946fd
rom100main <rom100main@proton.me>
2026-05-20 11:23:57 +0200

    feat: button for quick switcher

--- src/view/sidebar/header.rs
+++ src/view/sidebar/header.rs
@@ -6,21 +6,33 @@ use gpui_component::{
     sidebar::SidebarHeader,
 };
 
-use super::DatalithView;
 use super::super::palette::PaletteKind;
+use super::DatalithView;
 
 impl DatalithView {
     pub(crate) fn render_sidebar_header(&self, cx: &mut Context<Self>) -> impl IntoElement {
         SidebarHeader::new().p_2().child(
-            h_flex().w_full().justify_end().child(
-                Button::new("search-trigger")
-                    .ghost()
-                    .icon(IconName::Search)
-                    .on_click(cx.listener(|view, _, _, cx| {
-                        view.palette.open_as(PaletteKind::Search);
-                        cx.notify();
-                    })),
-            ),
+            h_flex()
+                .w_full()
+                .gap_1()
+                .child(
+                    Button::new("search-trigger")
+                        .ghost()
+                        .icon(IconName::Search)
+                        .on_click(cx.listener(|view, _, _, cx| {
+                            view.palette.open_as(PaletteKind::Search);
+                            cx.notify();
+                        })),
+                )
+                .child(
+                    Button::new("logo-trigger")
+                        .ghost()
+                        .icon(IconName::LayoutDashboard)
+                        .on_click(cx.listener(|view, _, _, cx| {
+                            view.palette.open_as(PaletteKind::QuickSwitcher);
+                            cx.notify();
+                        })),
+                ),
         )
     }
 }
