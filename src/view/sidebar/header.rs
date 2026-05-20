84a76495e139ff2dac860c8e010be17d731d1d47
rom100main <rom100main@proton.me>
2026-05-20 12:04:39 +0200

    feat: prettier action button

--- src/view/sidebar/header.rs
+++ src/view/sidebar/header.rs
@@ -1,38 +1,42 @@
 use gpui::*;
-use gpui_component::{
-    IconName,
-    button::{Button, ButtonVariants as _},
-    h_flex,
-    sidebar::SidebarHeader,
-};
+use gpui_component::{ActiveTheme, IconName, h_flex};
 
 use super::super::palette::PaletteKind;
 use super::DatalithView;
+use crate::consts::{BORDER_WIDTH, TREE_PADDING_PX};
 
 impl DatalithView {
     pub(crate) fn render_sidebar_header(&self, cx: &mut Context<Self>) -> impl IntoElement {
-        SidebarHeader::new().p_2().child(
-            h_flex()
-                .w_full()
-                .gap_1()
-                .child(
-                    Button::new("search-trigger")
-                        .ghost()
-                        .icon(IconName::Search)
-                        .on_click(cx.listener(|view, _, _, cx| {
-                            view.palette.open_as(PaletteKind::Search);
-                            cx.notify();
-                        })),
-                )
-                .child(
-                    Button::new("logo-trigger")
-                        .ghost()
-                        .icon(IconName::LayoutDashboard)
-                        .on_click(cx.listener(|view, _, _, cx| {
-                            view.palette.open_as(PaletteKind::QuickSwitcher);
-                            cx.notify();
-                        })),
-                ),
-        )
+        div()
+            .px_2()
+            .py_2()
+            .pl(px(TREE_PADDING_PX))
+            .border_b(px(BORDER_WIDTH))
+            .border_color(cx.theme().border)
+            .child(
+                h_flex()
+                    .w_full()
+                    .gap_2()
+                    .child(
+                        div()
+                            .id("search-trigger")
+                            .cursor_pointer()
+                            .on_click(cx.listener(|view, _, _, cx| {
+                                view.palette.open_as(PaletteKind::Search);
+                                cx.notify();
+                            }))
+                            .child(IconName::Search),
+                    )
+                    .child(
+                        div()
+                            .id("switcher-trigger")
+                            .cursor_pointer()
+                            .on_click(cx.listener(|view, _, _, cx| {
+                                view.palette.open_as(PaletteKind::QuickSwitcher);
+                                cx.notify();
+                            }))
+                            .child(IconName::LayoutDashboard),
+                    ),
+            )
     }
 }
