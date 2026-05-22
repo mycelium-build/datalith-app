f398f3e8e087ecc4cd66bd83ab61b24af1bc9f97
rom100main <rom100main@proton.me>
2026-05-23 00:52:45 +0200

    fix: display shortcuts on MacOS in OS menu item

--- src/main.rs
+++ src/main.rs
@@ -107,6 +107,34 @@ fn main() {
         cx.on_action(go_back);
         cx.on_action(go_forward);
         cx.on_action(handle_open_link);
+        cx.bind_keys([
+            KeyBinding::new("cmd-n", NewFile, None),
+            KeyBinding::new("cmd-shift-n", NewFolder, None),
+            KeyBinding::new("f2", Rename, None),
+            KeyBinding::new("cmd-backspace", Delete, None),
+            KeyBinding::new("cmd-d", Duplicate, None),
+            KeyBinding::new("cmd-shift-e", OpenInExplorer, None),
+            KeyBinding::new("cmd-l", CopyPath, None),
+            KeyBinding::new("cmd-shift-f", ToggleSearch, None),
+            KeyBinding::new("cmd-p", ToggleQuickSwitcher, None),
+            KeyBinding::new("cmd-t", NewTab, None),
+            KeyBinding::new("cmd-w", CloseTab, None),
+            KeyBinding::new("cmd-0", FocusSidebar, None),
+            KeyBinding::new("cmd-shift-d", ToggleTheme, None),
+            KeyBinding::new("cmd-1", SelectTab1, None),
+            KeyBinding::new("cmd-2", SelectTab2, None),
+            KeyBinding::new("cmd-3", SelectTab3, None),
+            KeyBinding::new("cmd-4", SelectTab4, None),
+            KeyBinding::new("cmd-5", SelectTab5, None),
+            KeyBinding::new("cmd-6", SelectTab6, None),
+            KeyBinding::new("cmd-7", SelectTab7, None),
+            KeyBinding::new("cmd-8", SelectTab8, None),
+            KeyBinding::new("cmd-9", SelectTab9, None),
+            KeyBinding::new("cmd-e", ToggleEditorMode, None),
+            KeyBinding::new("cmd-[", GoBack, None),
+            KeyBinding::new("cmd-]", GoForward, None),
+            KeyBinding::new("cmd-enter", OpenLink, None),
+        ]);
         cx.set_menus([
             Menu::new("File").items([
                 MenuItem::action("New File", NewFile),
@@ -135,34 +163,6 @@ fn main() {
                 MenuItem::separator(),
                 MenuItem::action("Settings", OpenSettings),
             ]),
-        ]);
-        cx.bind_keys([
-            KeyBinding::new("cmd-n", NewFile, None),
-            KeyBinding::new("cmd-shift-n", NewFolder, None),
-            KeyBinding::new("f2", Rename, None),
-            KeyBinding::new("cmd-backspace", Delete, None),
-            KeyBinding::new("cmd-d", Duplicate, None),
-            KeyBinding::new("cmd-shift-e", OpenInExplorer, None),
-            KeyBinding::new("cmd-l", CopyPath, None),
-            KeyBinding::new("cmd-shift-f", ToggleSearch, None),
-            KeyBinding::new("cmd-p", ToggleQuickSwitcher, None),
-            KeyBinding::new("cmd-t", NewTab, None),
-            KeyBinding::new("cmd-w", CloseTab, None),
-            KeyBinding::new("cmd-0", FocusSidebar, None),
-            KeyBinding::new("cmd-shift-d", ToggleTheme, None),
-            KeyBinding::new("cmd-1", SelectTab1, None),
-            KeyBinding::new("cmd-2", SelectTab2, None),
-            KeyBinding::new("cmd-3", SelectTab3, None),
-            KeyBinding::new("cmd-4", SelectTab4, None),
-            KeyBinding::new("cmd-5", SelectTab5, None),
-            KeyBinding::new("cmd-6", SelectTab6, None),
-            KeyBinding::new("cmd-7", SelectTab7, None),
-            KeyBinding::new("cmd-8", SelectTab8, None),
-            KeyBinding::new("cmd-9", SelectTab9, None),
-            KeyBinding::new("cmd-e", ToggleEditorMode, None),
-            KeyBinding::new("cmd-[", GoBack, None),
-            KeyBinding::new("cmd-]", GoForward, None),
-            KeyBinding::new("cmd-enter", OpenLink, None),
         ]);
 
         let last_folder = load_last_folder();
