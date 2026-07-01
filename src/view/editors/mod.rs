c8230e31c7f312e6d5edcb399aa1466326f7387d
rom100main <rom100main@proton.me>
2026-07-01 16:29:52 +0200

    feat(todo-txt): map todo txt editor

--- src/view/editors/mod.rs
+++ src/view/editors/mod.rs
@@ -1,15 +1,18 @@
 pub(crate) mod markdown;
 pub(crate) mod plain_text;
+pub(crate) mod todo_txt;
 
 use gpui::*;
 use gpui_component::input::InputState;
 
 use self::markdown::MarkdownEditor;
 use self::plain_text::PlainTextEditor;
+use self::todo_txt::TodoTxtEditor;
 
 pub(crate) enum EditorKind {
     Markdown(MarkdownEditor),
     PlainText(PlainTextEditor),
+    TodoTxt(TodoTxtEditor),
 }
 
 impl EditorKind {
@@ -17,6 +20,7 @@ impl EditorKind {
         match self {
             EditorKind::Markdown(editor) => editor.render(cx),
             EditorKind::PlainText(editor) => editor.render(cx),
+            EditorKind::TodoTxt(editor) => editor.render(cx),
         }
     }
 
@@ -24,6 +28,7 @@ impl EditorKind {
         match self {
             EditorKind::Markdown(editor) => editor.focus_handle(cx),
             EditorKind::PlainText(editor) => editor.focus_handle(cx),
+            EditorKind::TodoTxt(editor) => editor.focus_handle(cx),
         }
     }
 
@@ -31,6 +36,7 @@ impl EditorKind {
         match self {
             EditorKind::Markdown(editor) => Some(editor.input()),
             EditorKind::PlainText(editor) => Some(editor.input()),
+            EditorKind::TodoTxt(_) => None,
         }
     }
 
