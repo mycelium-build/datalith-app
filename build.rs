a5b4b201425271a71e6c06d6bc237fa687c95ac2
lmandrelli <73400538+lmandrelli@users.noreply.github.com>
2026-08-09 20:16:14 +0000

    chore: apply rustfmt

--- build.rs
+++ build.rs
@@ -3,8 +3,7 @@ fn main() -> Result<(), embed_resource::CompilationResult> {
     println!("cargo:rerun-if-changed=assets/icons/datalith.ico");
     println!("cargo:rerun-if-changed=assets/icons/datalith.rc");
 
-    embed_resource::compile("assets/icons/datalith.rc", embed_resource::NONE)
-        .manifest_required()
+    embed_resource::compile("assets/icons/datalith.rc", embed_resource::NONE).manifest_required()
 }
 
 #[cfg(not(target_os = "windows"))]
