05e1e55948498a143f5414fb572c84a4a404f4ca
rom100main <rom100main@proton.me>
2026-08-10 18:08:25 +0200

    feat(design): new path to app icon

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 #[cfg(target_os = "windows")]
 fn main() -> Result<(), embed_resource::CompilationResult> {
-    println!("cargo:rerun-if-changed=assets/icons/datalith.ico");
-    println!("cargo:rerun-if-changed=assets/icons/datalith.rc");
+    println!("cargo:rerun-if-changed=assets/datalith.ico");
+    println!("cargo:rerun-if-changed=assets/datalith.rc");
 
-    embed_resource::compile("assets/icons/datalith.rc", embed_resource::NONE).manifest_required()
+    embed_resource::compile("assets/datalith.rc", embed_resource::NONE).manifest_required()
 }
 
 #[cfg(not(target_os = "windows"))]
