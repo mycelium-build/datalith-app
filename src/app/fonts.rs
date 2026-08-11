5319afb22771275ccafeb463dc0b254044145d71
rom100main <rom100main@proton.me>
2026-08-11 18:55:10 +0200

    feat(design): Pixeloid font files

--- src/app/fonts.rs
+++ src/app/fonts.rs
@@ -5,8 +5,8 @@ use gpui::App;
 pub const PIXELOID_FONT: &str = "Pixeloid Sans";
 
 pub fn load_embedded_fonts(cx: &App) {
-    static REGULAR: &[u8] = include_bytes!("../../assets/fonts/PixeloidSans.ttf");
-    static BOLD: &[u8] = include_bytes!("../../assets/fonts/PixeloidSans-Bold.ttf");
+    static REGULAR: &[u8] = include_bytes!("../../assets/fonts/Pixeloid/PixeloidSans.ttf");
+    static BOLD: &[u8] = include_bytes!("../../assets/fonts/Pixeloid/PixeloidSans-Bold.ttf");
     cx.text_system()
         .add_fonts(vec![Cow::Borrowed(REGULAR), Cow::Borrowed(BOLD)])
         .ok();
